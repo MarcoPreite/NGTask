44bdee432514a5aed324274b59323a721f93578d
Marco Preite <preite.marco93@gmail.com>
2022-03-11 11:48:51 +0100

    Commit first part of NGTask

---
+++ NGTask/ngprj/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectContainerComponent } from './project/project-container/project-container.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { ProjectListComponent } from './project/project-list/project-list.component';
+import { ProjectFormComponent } from './project/project-form/project-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectContainerComponent,
+    ProjectDetailComponent,
+    ProjectListComponent,
+    ProjectFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ NGTask/ngprj/src/app/models/Project.ts
@@ -0,0 +1,15 @@
+import Task from './Task';
+
+type priority = 'low' | 'medium' | 'high';
+
+export interface Project {
+    id: number;
+    code: string;
+    name: string;
+    description?: string;
+    start: Date;
+    end?: Date;
+    priority: priority,
+    done: boolean;
+    tasks: Task[]
+}
